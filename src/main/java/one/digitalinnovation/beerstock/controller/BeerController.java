7602c165cc6cc8639201dc3233b25e9c02bf75cb
Raphael Paulino <raphael.paulino.web@gmail.com>
2021-10-02 18:48:56 -0300

    - fixed null value on making a post request

--- src/main/java/one/digitalinnovation/beerstock/controller/BeerController.java
+++ src/main/java/one/digitalinnovation/beerstock/controller/BeerController.java
@@ -1,6 +1,5 @@
 package one.digitalinnovation.beerstock.controller;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.AllArgsConstructor;
 import one.digitalinnovation.beerstock.dto.BeerDTO;
 import one.digitalinnovation.beerstock.exception.BeerAlreadyRegisteredException;
@@ -8,11 +7,15 @@ import one.digitalinnovation.beerstock.repository.BeerRepository;
 import one.digitalinnovation.beerstock.service.BeerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/beers")
